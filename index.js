e0755014d3eb668067bdb6b18ad96e955ae7b31c
LeoRiether <leonardoalves8217@gmail.com>
2024-12-30 23:45:48 -0300

    Push non-debug version

--- index.js
+++ index.js
@@ -15,6 +15,69 @@ let html = (tag, props, ...children) => {
 
 let text = t => document.createTextNode(t);
 
+// Thanks StackOverflow (and past me)
+// https://github.com/LeoRiether/Fireworks2019.5JS/blob/master/countdown.js
+// Thanks, StackOverflow
+let MyDate = (() => {
+    let offset = 0;
+
+    const worldTimeAPI = {
+        url: "https://worldtimeapi.org/api/timezone/Etc/UTC",
+        timeParam: "utc_datetime",
+    };
+
+    const worldClockAPI = {
+        url: "http://worldclockapi.com/api/json/utc/now",
+        timeParam: "currentDateTime",
+    };
+
+    const anotherWorldTimeAPI = {
+        url: "https://myworldtimeapi.herokuapp.com/Antarctica/Troll",
+        timeParam: "utc_datetime",
+    };
+
+    const getOffsetWith = API => new Promise((res, rej) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", API.url);
+        xhr.responseType = 'json';
+
+        xhr.onload = () => {
+            let server = new Date(xhr.response[API.timeParam]);
+            res(server - new Date());
+        };
+
+        xhr.onerror = rej;
+
+        xhr.send();
+    });
+
+    const getOffset = () =>
+        getOffsetWith(anotherWorldTimeAPI)
+            .catch(() => {
+                console.log("myworldtimeapi query failed! Trying worldclocktime instead");
+                return getOffsetWith(worldTimeAPI);
+            })
+            .catch(() => {
+                console.log("worldtimeapi query failed! Trying worldclockapi instead");
+                return getOffsetWith(worldClockAPI);
+            });
+
+    (function init() {
+        getOffset()
+            .then(o => offset = o)
+            .then(() => console.log('offset found: ', offset))
+            .catch(() => console.log('getOffset() failed'));
+    })();
+
+    function now() {
+        let d = new Date();
+        d.setTime(d.getTime() + offset);
+        return d;
+    }
+
+    return { now };
+})();
+
 let variables = {
     container: document.getElementById('variables'),
 
@@ -247,7 +310,7 @@ function pushCountdown(seconds) {
 
 let ttp = 0;
 let targetDate = new Date(2025, 0, 1, 0, 0, 0);
-targetDate = new Date(2024, 11, 30, 23, 40, 0);
+// targetDate = new Date(2024, 11, 30, 23, 40, 0);
 let beforeMidnight = true;
 let lastSecondsTillMidnight;
 function update(dt) {
@@ -272,7 +335,7 @@ function update(dt) {
     }
 
     if (beforeMidnight) {
-        let secondsTillMidnight = ~~((targetDate - Date.now()) / 1000);
+        let secondsTillMidnight = ~~((targetDate - MyDate.now()) / 1000);
         if (secondsTillMidnight <= 0) {
             for (let i = 0; i < 5; i++)
                 setTimeout(() => pushText("2025"), i*1000);
